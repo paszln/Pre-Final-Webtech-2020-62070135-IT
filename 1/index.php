22643fc1493a390c326f21e51ed451e9e7fc9fef
paszln <56357821+paszln@users.noreply.github.com>
2020-11-18 21:47:48 +0700

    Update index.php

--- 1/index.php
+++ 1/index.php
@@ -11,6 +11,11 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
+<style>
+    body{
+        background-color:aliceblue;
+    }
+</style>
 <body>
     <?php
         $url = "https://dd-wtlab2020.netlify.app/pre-final/ezquiz.json";
@@ -19,7 +24,8 @@
 
         for ($i = 0; $i < sizeOf($data->tracks->items); $i++){
             $song = $data->tracks->items[$i]->album;
-            echo "<div class='card' style='width:640px;'>";
+            echo "<center>";
+            echo "<div class='card' style='width:640px; margin-top:30px;'>";
             echo "<img class='card-img-top' style='width:640px;height:640px;' src='".$song->images[0]->url."'>";
             echo "<div class='card-body'>";
             echo "<h5 class='card-title'>" .$song->name."</h5>";
@@ -27,7 +33,8 @@
             echo "<p class='card-text'>Release date: ".$song->release_date."</p>";
             echo "<p class='card-text'>Avaliable: ".sizeof($song->available_markets)." countries</p>";
             echo "</div>";
-            echo "</div><br>";
+            echo "</div><br><br>";
+            echo "</center>";
         }
 
     ?>
